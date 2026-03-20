aa3604d75b820a464ac546441e017ac3c03aba06
yangsai15 <56031660+yangsai15@users.noreply.github.com>
2026-03-20 16:12:37 +0800

    add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/index.ts',
+  ],
+};
